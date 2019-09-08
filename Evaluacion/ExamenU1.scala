c5bca454aef0705d5d463255de304edb378996dd
joseomarvite <jose.vite@tectijuana.edu.mx>
2019-09-07 18:24:26 -0700

    Se comento

--- Evaluacion/ExamenU1.scala
+++ Evaluacion/ExamenU1.scala
@@ -1,4 +1,4 @@
-val scores = Array(3,4,21,36,10,28,35,5,24,42)
+val puntajes = Array(3,4,21,36,10,28,35,5,24,42)
 
 // Se agrego un funcion que recibira un arreglo de numeros enteros (Int)
 def breakingRecords (scores:Array[Int]):=
