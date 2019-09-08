6d2987571766fcd6e8c8a48460cbd74b888f1954
joseomarvite <jose.vite@tectijuana.edu.mx>
2019-09-07 18:26:41 -0700

    Se comento

--- Evaluacion/ExamenU1.scala
+++ Evaluacion/ExamenU1.scala
@@ -1,38 +1,39 @@
 val puntajes = Array(3,4,21,36,10,28,35,5,24,42)
 
 // Se agrego un funcion que recibira un arreglo de numeros enteros (Int)
-def breakingRecords (scores:Array[Int]):=
+def breakingRecords(puntajes:Array[Int]):=
 {
-     // Se agrego el score, que sera el contador que se ira moviento en los espacios del arreglo
+     // Se agrego el puntaje, que sera el contador que se ira moviento en los espacios del arreglo
      var score = 0
      // La funcion (.head) nos da la cabeza del arreglo
-     var max = scores.head
-     var min = scores.head
+     var max = puntajes.head
+     var min = puntajes.head
      // Esta variable llevaran el conteo de puntaje alto 
-     var higherScore = 0
+     var puntajeAlto = 0
      // Esta variable llevaran el conteo de puntaje bajo
-     var lowerScore = 0
+     var puntajeMenor = 0
      // Se crea un (foreach) en el arreglo, que ira comparando la variable (score)
      // en cada espacio del arreglo
-     scores.foreach(score => {
+     puntajes.foreach(score => {
           // Si el score es mayor a la cabeza del arreglo, entonces ese tomara el valor 
           // y se agregara al puntaje mas alto, el cual se ira sumando conforme
           // pase todos los elementos del arreglo
           if (score > max) {
                max = score
-               higherScore = higherScore + 1
+               println("Puntaje alto: " + score)
+               puntajeAlto = puntajeAlto + 1
           }
           // Si el score es menor a la cabeza del arreglo, entonces ese tomara el valor 
           // y se agregara al puntaje mas alto, el cual se ira sumando conforme
           // pase todos los elementos del arreglo
           if (score < min) {
                min = score
-               lowerScore = lowerScore + 1
+               println("Puntaje menor: " + score)
+               puntajeMenor = puntajeMenor + 1
           } 
      })
      // Las suma de los puntajes seran enviados a la pantalla
-     println(higherScore + " " + lowerScore)
+     println(puntajeAlto + " " + puntajeMenor)
      
 }
 
-breakingRecords(scores)
